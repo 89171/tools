b4e93db10d85027eb77a8ce97d23f00403929291
jianminzhu <jianminzhu@ctrip.com>
2021-04-30 15:52:52 +0800

    抽取getOption

--- src/img2webp.js
+++ src/img2webp.js
@@ -1,16 +1,12 @@
 #!/usr/bin/env node
 const sharp = require('sharp')
 const path = require('path')
-const { supportImages } = require('../util')
+const { getOption,supportImages } = require('../util')
 
-let params = process.argv.slice(2)
-
-const pFlagIndex = params.indexOf('-p')
-const tempArr = pFlagIndex > -1 && params.splice(pFlagIndex,2)
-const qualityString = +(tempArr && tempArr[1])
+const {option:qualityString,list} = getOption('-p')
 const quality = +qualityString ? (+qualityString >= 100 ? 100 : (+qualityString<=30 ? 75 : +qualityString)) :  75
-console.log(quality)
-params.forEach(item=>{
+
+list.forEach(item=>{
     const filePath = path.resolve(item)
     console.log(filePath)
     if(supportImages.indexOf(path.extname(filePath)) === -1) return;
--- util.js
+++ util.js
@@ -1,5 +1,17 @@
 const supportImages = ['.png','.jpg','.jpeg','.gif','.svg']
+const params = process.argv.slice(2)
+
+const getOption = (option) => {
+    const list = params.slice(0)
+    const flagIndex = list.indexOf(option)
+    const tempArr = flagIndex > -1 && list.splice(flagIndex,2)
+    return {
+        option: tempArr && tempArr[1],
+        list
+    }
+} 
 
 module.exports = {
-    supportImages
+    supportImages,
+    getOption
 }
